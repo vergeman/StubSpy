9806c90636d79016b871ccc138ee04f5d22fe75f
vergeman <alan.verga@gmail.com>
2011-10-21 20:27:23 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StubSpy::Application.config.secret_token = '8506e5d2b35dca2c02544e9f6740d39499041158f19d4e39f086c78f3fb5598020b90f4242d23c78f80cbc2ed6b05bd4a84ff03a2df4985ee7cd61d37bedf202'
