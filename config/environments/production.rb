ab828407b50d3d802dd1c66bf641f3309c20febf
vergeman <alan.verga@gmail.com>
2012-01-04 19:57:48 -0500

    compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,9 +14,8 @@ StubSpy::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
 
-  config.assets.precompile=true
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
